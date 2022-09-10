7f3a9f1c78a7d48235f154f783b287bfbeae7c16
RedBaron1918 <asatianikotiko1@gmail.com>
2022-09-10 17:25:28 +0400

    first part finished

--- index.js
+++ index.js
@@ -13,10 +13,31 @@ let w = document.documentElement.clientWidth;
             <img src="./images/magic.png" alt="roomba" />
 
         </div>`
-        console.log(document.documentElement.clientWidth)
-        console.log(w)
         document.getElementById('main-img').style.display = "none";
         document.getElementById('Main-text').innerText = "MagicMassage™ provides relief from head tensions, muscle aches, sleep irregularities and more"
+        document.getElementById('Main-btn').innerHTML=`
+        <div class="main-img-div">
+        <div class="div-img">
+        <img src="./images/picture1.png" alt="">
+    </div>
+    <div class="div-text">
+        <p>“It feels amazing on my head and it soothes muscle tension. “</p>
+        <div class="div-rate">
+            <div class="stars">
+                <i class="fa-solid fa-star" style="color: #F6C644; font-size: 12px;"></i>
+                 <i class="fa-solid fa-star" style="color: #F6C644; font-size: 12px;"></i>
+                 <i class="fa-solid fa-star" style="color: #F6C644; font-size: 12px;"></i>
+                 <i class="fa-solid fa-star" style="color: #F6C644; font-size: 12px;"></i>
+                 <i class="fa-solid fa-star" style="color: #F6C644; font-size: 12px;"></i>
+                 <p>Debra N.</p>
+            </div>
+            <div class="verify">
+                <i class="fa-solid fa-circle-check" style="color: #197AFF;"></i>
+                <p>VERIFIED BUYER</p>
+            </div>
+        `
+        document.getElementById('get-div').style.display = 'none';
+
     }else{
         document.getElementById("heading").innerHTML =`
         <h1>All-in-one massager that works like <span class="span">magic!
@@ -27,6 +48,9 @@ let w = document.documentElement.clientWidth;
         document.getElementById('main-img').style.display = "block";
         document.getElementById('Main-text').innerText = `
         MagicMassage™ provides relief from head tensions, muscle aches, sleep irregularities and more - suitable for adults, kids, and pets!
+        `
+        document.getElementById('get-div').style.display = 'flex';
+        document.getElementById('Main-btn').innerHTML=`
         `
     }
 }
